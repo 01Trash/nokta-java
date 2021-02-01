cafa7d405f094ffaa57e455d81af3bf78437ea3e
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-02-01 19:59:08 +0300

    Update HelloWolrd.java

--- HelloWolrd.java
+++ HelloWolrd.java
@@ -4,6 +4,6 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World and JAVA");
+        System.out.println("Hello World and JAVA!");
     }
 }
