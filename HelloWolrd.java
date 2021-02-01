770d281c8c4d021b9facda0d87f8d7368f56f50f
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-02-01 19:57:36 +0300

    Create HelloWolrd.java

---
+++ HelloWolrd.java
@@ -0,0 +1,9 @@
+// İlk JAVA deneyimim
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World and JAVA");
+    }
+}
